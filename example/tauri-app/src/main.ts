ae804e69db285356f338f2f78388e67e4883bd10
WSH032 <614337162@qq.com>
2024-12-24 11:34:49 +0800

    feat(example): python command ipc

--- example/tauri-app/src/main.ts
+++ example/tauri-app/src/main.ts
@@ -1,14 +1,28 @@
 import { invoke } from "@tauri-apps/api/core";
+import { pyInvoke } from "tauri-plugin-pytauri-api";
+// or if tauri config `app.withGlobalTauri = true`:
+//
+// ```js
+// const { pyInvoke } = window.__TAURI__.pytauri;
+// ```
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
+interface Greeting {
+  message: string;
+}
+
 async function greet() {
   if (greetMsgEl && greetInputEl) {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
+    const rsGreeting = await invoke<string>("greet", {
       name: greetInputEl.value,
     });
+    const pyGreeting = await pyInvoke<Greeting>("greet", {
+      name: greetInputEl.value,
+    });
+    greetMsgEl.textContent = rsGreeting + "\n" + pyGreeting.message;
   }
 }
 
