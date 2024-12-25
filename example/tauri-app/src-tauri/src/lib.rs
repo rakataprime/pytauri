fa23a81b94337183453198ce57bbd55ab235c514
WSH032 <614337162@qq.com>
2024-12-25 10:03:21 +0800

    feat(example): bundle python to build standalone app

--- example/tauri-app/src-tauri/src/lib.rs
+++ example/tauri-app/src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ use pytauri::standalone::{
     append_ext_mod, get_python_executable_from_venv, prepare_freethreaded_python_with_executable,
     write_py_err_to_file,
 };
+use tauri::{Builder, Manager as _};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -38,42 +39,73 @@ mod ext_mod {
 }
 
 fn prepare_python_interpreter() {
-    // See:
-    //
-    // - <https://github.com/PyO3/pyo3/issues/3589>
-    // - <https://github.com/PyO3/pyo3/issues/1896>
-    //
-    // pyo3 currently cannot automatically detect the virtual environment and configure pyconfig,
-    // so we do it manually here.
-    if cfg!(debug_assertions) {
+    // `cfg(dev)` is set by `tauri-build` in `build.rs`, which means running with `tauri dev`,
+    // see: <https://github.com/tauri-apps/tauri/pull/8937>.
+    if cfg!(dev) {
+        // virtualenv Python
+        //
+        // See:
+        //
+        // - <https://github.com/PyO3/pyo3/issues/3589>
+        // - <https://github.com/PyO3/pyo3/issues/1896>
+        //
+        // pyo3 currently cannot automatically detect the virtual environment and configure pyconfig,
+        // so we do it manually here.
         let venv_path = var("VIRTUAL_ENV").expect(
-            "The app is running in debug mode, \
+            "The app is running in tauri dev mode, \
                 please activate the python virtual environment first \
-                or set the VIRTUAL_ENV environment variable",
+                or set the `VIRTUAL_ENV` environment variable",
         );
         let python_executable = get_python_executable_from_venv(venv_path);
         prepare_freethreaded_python_with_executable(python_executable)
             .expect("failed to initialize python from venv");
     } else {
-        // embedded Python
-        todo!()
+        // embedded Python, i.e., bundle mode with `tauri build`.
+
+        // Actually, we don't use this app, we just use it to get the resource directory
+        let sample_app = Builder::default()
+            .build(tauri_generate_context())
+            .expect("failed to build sample app");
+        let resource_dir = sample_app
+            .path()
+            .resource_dir()
+            .expect("failed to get resource dir");
+
+        let py_executable = if cfg!(windows) {
+            resource_dir.join("python.exe")
+        } else {
+            resource_dir.join("bin/python3")
+        };
+
+        debug_assert!(
+            py_executable.is_file(),
+            "Python executable not found, maybe you forgot to bundle it: {}",
+            py_executable.display()
+        );
+
+        prepare_freethreaded_python_with_executable(py_executable)
+            .expect("failed to initialize embedded python");
     }
+}
+
+fn execute_python_script(py: Python<'_>) -> PyResult<()> {
+    // insert pytauri extension module into `sys.modules`
+    append_ext_mod(wrap_pymodule!(ext_mod)(py).into_bound(py))?;
+
+    // execute your Python script
+    py.run(
+        // equivalent to `python -m tauri_app`
+        c"from runpy import run_module; run_module('tauri_app')",
+        None,
+        None,
+    )
 }
 
 pub fn main() -> Result<(), PyErr> {
     prepare_python_interpreter();
 
     Python::with_gil(|py| {
-        // insert pytauri extension module into `sys.modules`
-        append_ext_mod(wrap_pymodule!(ext_mod)(py).into_bound(py))?;
-
-        // execute your Python script
-        let result = py.run(
-            // equivalent to `python -m tauri_app`
-            c"from runpy import run_module; run_module('tauri_app')",
-            None,
-            None,
-        );
+        let result = execute_python_script(py);
 
         // handle the error
         result.inspect_err(|e| {
